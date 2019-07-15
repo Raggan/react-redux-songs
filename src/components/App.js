7e39b348ee4dd426241f1c958d8b5ea3789c60ea
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-15 11:39:14 +0200

    Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        App
+        <SongList />
+        <SongDetail />
+      </div>
+    );
   }
 }
 
